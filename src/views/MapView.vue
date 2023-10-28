9686c1e5bb05325361ebfe6e16954ab120932250
Sukamakmur2023 <144505968+Sukamakmur2023@users.noreply.github.com>
2023-10-28 14:16:12 +0700

    Update peta batas desa

--- src/views/MapView.vue
+++ src/views/MapView.vue
@@ -101,7 +101,7 @@ export default {
     async loadSomeGeoJson() {
       const nextIndex = this.layerData.length;
 
-      const response = await fetch("/Desa-Sukamakmur.json");
+      const response = await fetch("Desa-Sukamakmur.json");
       const data = await response.json();
 
       console.log(data);
