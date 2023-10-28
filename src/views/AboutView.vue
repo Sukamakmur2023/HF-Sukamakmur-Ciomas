8609e419d3b8d14745e0b6a0a11977c4655ba11b
Sukamakmur2023 <144505968+Sukamakmur2023@users.noreply.github.com>
2023-10-28 14:41:01 +0700

    Update edit about

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -11,7 +11,7 @@ import Sidebar from "../components/Sidebar.vue";
     <Sidebar />
     <div class="flex-1 p-10">
       <p class="text-3xl font-semibold my-4">Struktur Desa</p>
-      <img src="@/assets/"1698471719688" alt="" />
+      <img src="@/assets/"Stuktur-Desa-Sukamakmur.png" alt="" />
     </div>
   </main>
 </template>
